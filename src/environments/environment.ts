0ddc71f106a82588434d36e164365e38bc237c0d
wiliam <williamhugogonzales@gmail.com>
2026-05-23 21:39:03 -0400

    feat: Vacunas y trim

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbxR0kLQ978wokYxzp38y2YS1VNWu5_OQI_kGfvinmM-5__1Tc4xafMvwzxVXVEAmMzP/exec'
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbycisM6u_ame_hbV8bd8SOPWBMqzL0P5TMlziP8vPi-NvPwjt8fUdKZx9LwLJoqgUI/exec'
 };
